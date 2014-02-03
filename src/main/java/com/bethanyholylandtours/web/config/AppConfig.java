3ed4ab90f5b1a6ef169db6690b2c870b997dbe3c
Teguh Wanrasagita <tsagita@hotmail.com>
2014-02-03 22:35:18 +0800

    Initial commit

---
+++ src/main/java/com/bethanyholylandtours/web/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.bethanyholylandtours.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by tsagita on 3/2/14.
+ */
+@Configuration
+public class AppConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
---
+++ src/main/java/com/bethanyholylandtours/web/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.bethanyholylandtours.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by tsagita on 3/2/14.
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String get(ModelMap model) {
+        return "index";
+    }
+}
